834a4b26c1d16288706e061949f6cc0c6fbd503a
Kristina <Bolshakovakristy@gmail.com>
2022-10-12 21:04:27 +0300

    Добавлено решение 1 задачи

---
+++ Homework_2/task_1.py
@@ -0,0 +1,27 @@
+# 1 – Напишите программу, которая принимает на вход вещественное число 
+# и показывает сумму его цифр.
+
+# Пример:
+# 6782 -> 23
+# 0,56 -> 11
+
+num = float(input('Введите число: '))
+num = float(abs(num))
+num_string = str(num)
+num_string = num_string.replace('.', '')
+sum = 0
+for i in num_string:
+    sum += int(i)
+print(f'Сумма цифр числа {num} =', sum)
+
+
+
+
+
+
+
+
+
+
+
+
