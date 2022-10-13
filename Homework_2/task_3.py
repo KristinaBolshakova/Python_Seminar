82a3fe34ff0ce2df8029c7dc79858a721572d52a
Kristina <Bolshakovakristy@gmail.com>
2022-10-13 11:12:25 +0300

    Добавлено решение задачи 3

--- Homework_2/task_3.py
+++ Homework_2/task_3.py
@@ -4,3 +4,11 @@
 # 1 -> 2.0
 # 2 -> 4.25
 # 3 -> 6.62037037037037
+
+n = int(input('Введите число n: '))
+sum = 0
+for i in range(1, n+1):
+    num = (1 + 1 / i) ** i
+    sum += num
+    print(sum)
+
