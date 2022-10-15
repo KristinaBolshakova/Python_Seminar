3c5b86b6a39b315c9199d4518e3bdea9fd42a9e0
Kristina <Bolshakovakristy@gmail.com>
2022-10-15 18:06:25 +0300

    Добавлены изменения

--- Homework_2/task_3.py
+++ Homework_2/task_3.py
@@ -7,7 +7,10 @@
 
 n = int(input('Введите число n: '))
 sum = 0
+num_list = []
+
 for i in range(1, n+1):             
     num = (1 + 1 / i) ** i
     sum += num
+    num_list.append(sum)
 print(sum)
