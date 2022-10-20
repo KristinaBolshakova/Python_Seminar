fdefedf81d939773aea49cc521d2d3573ab5037c
Kristina <Bolshakovakristy@gmail.com>
2022-10-20 12:33:03 +0300

    Добавлено решение 1 и 2 задачи

--- Homework_4/task_1.py
+++ Homework_4/task_1.py
@@ -1,3 +1,15 @@
 # Вычислить число c заданной точностью d
 # Пример:
 # при d = 0.001, π = 3.142 10^(-1) ≤ d ≤10^(-10)
+
+from decimal import Decimal
+
+num = input('Введите число: ')
+d = input('Введите степень округления: ')
+
+print(f'Число, до округления = {num}')
+print(f'Степень округдения d = {d}')
+
+number = Decimal(num)
+number = number.quantize(Decimal(d))
+print(f'Число после округления = {number}') 
--- Homework_4/task_2.py
+++ Homework_4/task_2.py
@@ -1,1 +1,21 @@
 # Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N.
+
+import math
+
+
+num = int(input('Введите число N: '))
+num_list = []
+
+while num % 2 == 0:
+    num_list.append(2)
+    # print(2)
+    num = num / 2
+for i in range(3, int(math.sqrt(num)) +1, 2):
+    while(num % i == 0):
+        num_list.append(i)
+        # print(i)
+        num = num / i
+if num>2:
+    num_list.append(int(num))
+    # print(num)
+print(f'Простые множетели числа N: {num_list}')
