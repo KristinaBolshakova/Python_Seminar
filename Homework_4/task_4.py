11b4a969dfd232093545a0e0a6b53c7c2432fec6
Kristina <Bolshakovakristy@gmail.com>
2022-10-21 11:03:59 +0300

    Добавлено решение 4 задачи и коррекция 3

--- Homework_4/task_4.py
+++ Homework_4/task_4.py
@@ -1,3 +1,50 @@
 # Задана натуральная степень k. Сформировать случайным образом список коэффициентов (значения от 0 до 100) многочлена и записать в файл многочлен степени k.
 # Пример:
 # k=2 => 2x^2 + 4x + 5 = 0 или x^2 + 5 = 0 или 10x^2 = 0
+
+from random import randint
+
+k = int(input('Введите натуральную степень k = '))
+data = open('Homework_4.txt', 'w')
+
+list_ratio = []
+new_list = []
+for i in range(k+1):
+    list_ratio.append(randint(0, 100))
+print(f'Список случайных коэффициентов: {list_ratio}')
+print()
+new_list.append(f'k = {k} => ')
+for i in list_ratio:
+    print(f'{i}*x**{k}', end = ' + ')
+    new_list.append(f'{i}*x**{k} + ')
+    k -= 1
+    if k == 0:
+        break
+print(f'{list_ratio[-1]}', end = ' = 0')
+new_list.append(f'{list_ratio[-1]} = 0' )
+print(new_list)
+
+data.writelines(new_list)
+data.close
+
+exit()
+        
+
+
+
+    
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
